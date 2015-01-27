1212f794f82ca7809e84bf8cacbd861bb8ba0c5e
Duane M. Moody <dmmoody@gmail.com>
2015-01-27 11:12:38 -0800

    add index route and products route

--- app.rb
+++ app.rb
@@ -5,5 +5,18 @@ also_reload("./lib/**/*.rb")
 require("./lib/product")
 require("pg")
 
+DB = PG.connect({:dbname => 'pos_development'})
+
 get("/") do
+  @products = Product.all()
+  erb(:index)
+end
+
+post('/products') do
+  name = params.fetch('name')
+  price = params.fetch('price')
+  status = params.fetch('status')
+  @product = Product.new({:name => name, :price => price, :status => status})
+  @product.save()
+  erb(:success)
 end
